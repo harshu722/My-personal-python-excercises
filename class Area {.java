4d6da91909127d4992983efb1d408c2fa724f145
Kolluru Saharsh <91141030+harshu722@users.noreply.github.com>
2023-03-19 02:31:16 +0530

    Add files via upload

---
+++ class Area {.java
@@ -0,0 +1,10 @@
+class Area {
+	public static void main(string args[]) {
+		double pi, r, a;
+		r = 10.8; // Radious of the circle
+		pi = 3.1416;
+		a = pi * r *r; // compute area 
+		system.out.println("Area of the circle is " + a);
+		
+	}
+}
